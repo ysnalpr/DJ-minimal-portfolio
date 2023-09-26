9cf69fecfd128f48dc1b5b51bb7a875d355e16b5
ysnalpr <yasinalipour01@gmail.com>
2023-09-26 19:26:28 +0330

    Added easy_thumbnails to use for images

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main.apps.MainConfig",
+    "easy_thumbnails",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -127,3 +130,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls", namespace="main")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 if settings.DEBUG:
